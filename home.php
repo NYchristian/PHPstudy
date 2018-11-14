f29e7d50c1336c1f0519ada9022b6b72e1620b81
rurarera <rurarerainc@yahoo.com>
2018-11-14 17:33:08 +0900

    Big HR added

--- home.php
+++ home.php
@@ -1,14 +1,15 @@
 <?php
    include('session.php');
 ?>
-<html">
+<html>
    
    <head>
       <title>Welcome </title>
    </head>
    
    <body>
-      <h1>Welcome <?php echo $login_session; ?></h1> 
+      <h1>Welcome <?php echo $login_session; ?></h1>
+      <h3>hr</h3> 
       <h2><a href = "logout.php">Sign Out</a></h2>
    </body>
    
